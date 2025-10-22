5412c4b5949e2e573ab09e085421afb6332a8112
kelmcao <kmcao@usc.edu>
2025-10-21 17:36:37 -0700

    Debugging

---
+++ project-2/sketch.js
@@ -0,0 +1,34 @@
+let fireSound;
+let hasStarted = false;
+
+function preload() {
+  fireSound = loadSound("audio\fire-1.mp3");
+}
+
+function setup() {
+  createCanvas(1, 1).hide();
+  console.log("Sketch loaded. Click anywhere to start audio.");
+}
+
+function mousePressed() {
+  if (!hasStarted) {
+    hasStarted = true;
+    fireSound.play();
+    // userStartAudio().then(() => {
+    //   fireSound.setVolume(0);
+    //   fireSound.loop();
+    //   fadeInSound(3);
+    //   console.log("Audio started!");
+    // }).catch(err => console.error("Playback blocked:", err));
+  }
+}
+
+function fadeInSound(duration) {
+  let currentVol = 0;
+  let step = 0.05;
+  let interval = setInterval(() => {
+    currentVol += step;
+    fireSound.setVolume(constrain(currentVol, 0, 1));
+    if (currentVol >= 1) clearInterval(interval);
+  }, duration * 1000 * step);
+}
