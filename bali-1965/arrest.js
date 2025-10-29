288c9df4a9ba58819d0badaeac81ef55f0f6718f
kelmcao <kmcao@usc.edu>
2025-10-28 17:45:27 -0700

    Update arrest.js

--- bali-1965/arrest.js
+++ bali-1965/arrest.js
@@ -4,20 +4,24 @@ function setup() {
     noCanvas();
 
     paragraph = select('.paragraph p');
-    selectAll('a').forEach(function(link) {
-        if (link.html().includes("silently watch him pass by")) {
-            link.mousePressed(function(event) {
+ 
+    select('body').elt.addEventListener('click', function(event) {
+        let target = event.target;
+        
+        if (target.tagName === 'A') {
+            let linkText = target.textContent;
+            
+            if (linkText.includes("silently watch him pass by")) {
                 event.preventDefault();
                 updateTextFirst();
-            });
-        }
-        if (link.html().includes("Wait-he hasn't done anything wrong")) {
-            link.mousePressed(function(event) {
+            }
+            else if (linkText.includes("Wait-he hasn't done anything wrong")) {
                 event.preventDefault();
                 updateTextSecond();
-            });
+            }
         }
     });
+    
     let audio = select('audio');
     audio.volume(0.4);
 }
@@ -28,14 +32,14 @@ function updateTextFirst() {
     });
 
     paragraph.html(`
-    <span id="fade" style="opacity:0; transition:opacity 1s;">
+    <span id="fade-text" style="opacity:0; transition:opacity 1s;">
       He glances briefly at you, before holding his head down in shame. 
       <a href="ending2.html">You keep moving.</a>
     </span>
   `);
 
     setTimeout(function() {
-        select('#fade').style('opacity', 1);
+        select('#fade-text').style('opacity', 1);
     }, 10);
 }
 
@@ -45,7 +49,7 @@ function updateTextSecond() {
     });
 
     paragraph.html(`
-    <span id="fade2" style="opacity:0; transition:opacity 1s;">
+    <span id="fade-text2" style="opacity:0; transition:opacity 1s;">
       The soldiers push you away, but you're insistent. Your professor looks down, averting your gaze.
       Your memory is blurry here-you recall the soldiers asking him "do you know this student?"
       He adamantly answers "No," and asks you to leave. Do you...
@@ -55,6 +59,6 @@ function updateTextSecond() {
   `);
 
     setTimeout(function() {
-        select('#fade2').style('opacity', 1);
+        select('#fade-text2').style('opacity', 1);
     }, 10);
 }
