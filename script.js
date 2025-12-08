c2eeeb98d7b38f46cd6d9f3e3d29fa5439efa4aa
kelmcao <kmcao@usc.edu>
2025-12-08 01:32:50 -0800

    final

--- script.js
+++ script.js
@@ -7,3 +7,41 @@ $(document).ready(function() {
     $target.slideToggle();
     });
 });
+
+let particles = [];
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    // Create particles for a gentle animated background
+    for (let i = 0; i < 20; i++) {
+        particles.push({
+            x: random(width),
+            y: random(height),
+            size: random(70, 300),
+            speedX: random(-0.5, 0.5),
+            speedY: random(-0.5, 0.5),
+            color: color(random([255, 221, 255]), random([240, 200, 255]), 255, random(30, 80))
+        });
+    }
+}
+
+function draw() {
+
+    background(255, 240, 245); // lavenderblush
+
+    drawingContext.filter = 'blur(20px)';
+
+    for (let p of particles) {
+        noStroke();
+        fill(p.color);
+        circle(p.x, p.y, p.size);
+        
+        p.x += p.speedX;
+        p.y += p.speedY;
+
+    }
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
