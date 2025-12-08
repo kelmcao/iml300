8cea9327ccf4c716adc03a2936f7ab83741af31c
kelmcao <kmcao@usc.edu>
2025-12-08 00:50:55 -0800

    changes for final

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('[data-p5]').click(function() {
+    var targetId = $(this).data('p5');
+    var $target = $('[data-p5-id="' + targetId + '"]');
+    
+    $('.p5-container').not($target).slideUp();
+    $target.slideToggle();
+    });
+});
